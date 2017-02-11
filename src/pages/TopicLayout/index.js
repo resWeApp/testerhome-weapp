e65fb55959fa7d4b55e1f42699c3ac73a859fd35
pennyjs <pennyjs@mbhq.io>
2017-02-11 23:01:48 +0800

    pennyjs <pennyjs@mbhq.io>

---
+++ src/pages/TopicLayout/index.js
@@ -0,0 +1,13 @@
+import i18n from '../../utils/i18n'
+
+module.exports = Page({
+  data: {
+  },
+  onReady() {
+    wx.setNavigationBarTitle({
+      title: i18n.zh_CN.explore
+    })
+  },
+  onLoad() {
+  }
+})
